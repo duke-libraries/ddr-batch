6776a355cb179a6ee9334aedab61bd4bb48f00b5
Jim Coble <jim.coble@duke.edu>
2015-10-29 17:21:09 -0400

    Batch relationship objects can now point to other objects in the batch.
    They can also point directly to repository objects but the addition of this option eliminates the
    need to pre-minted repository ID's to use as the objects of relationships.

--- spec/models/batch_spec.rb
+++ spec/models/batch_spec.rb
@@ -39,19 +39,17 @@ module Ddr::Batch
     context "validate" do
       let(:parent) { FactoryGirl.create(:test_parent) }
       let(:pid_cache) { { parent.pid => parent.class.name} }
-      before do
+      it "should cache the results of looking up relationship objects" do
+        expect(batch).to receive(:add_found_pid).once.with(parent.pid, "TestParent").and_call_original
         batch.batch_objects.each do |obj|
           obj.batch_object_relationships <<
               BatchObjectRelationship.new(
                   :name => BatchObjectRelationship::RELATIONSHIP_PARENT,
                   :object => parent.pid,
-                  :object_type => BatchObjectRelationship::OBJECT_TYPE_PID,
+                  :object_type => BatchObjectRelationship::OBJECT_TYPE_REPO_ID,
                   :operation => BatchObjectRelationship::OPERATION_ADD
-                  )
+              )
         end
-      end
-      it "should cache the results of looking up relationship objects" do
-        expect(batch).to receive(:add_found_pid).once.with(parent.pid, "TestParent").and_call_original
         batch.validate
         expect(batch.found_pids).to eq(pid_cache)
       end
