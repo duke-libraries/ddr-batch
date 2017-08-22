d481e5948c08c59bf288df598c6b76b840c2f678
Jim Coble <jim.coble@duke.edu>
2017-08-22 10:29:57 -0400

    Skip structue updates during batch ingest; DDR-1149.
    Structure updates based on a batch ingest are handled after the batch completes by a
    notification subscriber in DulHydra (SetDefaultStructuresAfterSuccessfulBatchIngest).

--- Gemfile
+++ Gemfile
@@ -5,3 +5,4 @@ gemspec
 
 gem "devise"
 gem "rails", "4.2.7"
+gem "ddr-models", git: "https://github.com/duke-libraries/ddr-models", ref: "a7765bbd60c389440dfc0e50d0f13cbbb1d81df3"
--- app/models/ddr/batch/ingest_batch_object.rb
+++ app/models/ddr/batch/ingest_batch_object.rb
@@ -97,11 +97,11 @@ module Ddr::Batch
         repo_object = model.constantize.new(:pid => repo_pid)
         repo_object.label = label if label
         batch_object_attributes.each { |a| repo_object = add_attribute(repo_object, a) }
-        repo_object.save(validate: false)
+        repo_object.save(validate: false, skip_structure_updates: true)
         batch_object_datastreams.each { |d| repo_object = populate_datastream(repo_object, d) }
         batch_object_relationships.each { |r| repo_object = add_relationship(repo_object, r) }
         batch_object_roles.each { |r| repo_object = add_role(repo_object, r) }
-        repo_object.save!
+        repo_object.save!(skip_structure_updates: true)
       rescue Exception => e1
         logger.fatal("Error in creating repository object #{repo_object.pid} for #{identifier} : #{e1}")
         if repo_object && !repo_object.new_record?
