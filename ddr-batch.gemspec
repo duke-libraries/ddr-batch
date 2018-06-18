29cf69c29b237c3f181f8ff3703dcd19eead5647
Jim Coble <jim.coble@duke.edu>
2018-06-18 10:35:58 -0400

    upgrade ddr-models dependency

--- ddr-batch.gemspec
+++ ddr-batch.gemspec
@@ -19,7 +19,7 @@ Gem::Specification.new do |s|
   s.add_dependency "rails", "~> 4.1"
   s.add_dependency "devise", "~> 3.4"
   s.add_dependency "paperclip", "~> 5.2"
-  s.add_dependency "ddr-models", "~> 2.10"
+  s.add_dependency "ddr-models", "~> 2.11"
 
   s.add_development_dependency "sqlite3"
   s.add_development_dependency "rspec-rails", "~> 3.1"
--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -15,25 +15,25 @@ ActiveRecord::Schema.define(version: 20171116183514) do
 
   create_table "batch_object_attributes", force: :cascade do |t|
     t.integer  "batch_object_id"
-    t.string   "datastream"
-    t.string   "name"
-    t.string   "operation"
+    t.string   "datastream",      limit: 255
+    t.string   "name",            limit: 255
+    t.string   "operation",       limit: 255
     t.text     "value",           limit: 65535
-    t.string   "value_type"
+    t.string   "value_type",      limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
   create_table "batch_object_datastreams", force: :cascade do |t|
     t.integer  "batch_object_id"
-    t.string   "operation"
-    t.string   "name"
+    t.string   "operation",       limit: 255
+    t.string   "name",            limit: 255
     t.text     "payload"
-    t.string   "payload_type"
+    t.string   "payload_type",    limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "checksum"
-    t.string   "checksum_type"
+    t.string   "checksum",        limit: 255
+    t.string   "checksum_type",   limit: 255
   end
 
   create_table "batch_object_messages", force: :cascade do |t|
@@ -46,54 +46,54 @@ ActiveRecord::Schema.define(version: 20171116183514) do
 
   create_table "batch_object_relationships", force: :cascade do |t|
     t.integer  "batch_object_id"
-    t.string   "name"
-    t.string   "operation"
-    t.string   "object"
-    t.string   "object_type"
+    t.string   "name",            limit: 255
+    t.string   "operation",       limit: 255
+    t.string   "object",          limit: 255
+    t.string   "object_type",     limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
   create_table "batch_object_roles", force: :cascade do |t|
     t.integer  "batch_object_id"
-    t.string   "operation"
-    t.string   "agent"
-    t.string   "role_type"
-    t.string   "role_scope"
+    t.string   "operation",       limit: 255
+    t.string   "agent",           limit: 255
+    t.string   "role_type",       limit: 255
+    t.string   "role_scope",      limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
   create_table "batch_objects", force: :cascade do |t|
     t.integer  "batch_id"
-    t.string   "identifier"
-    t.string   "model"
-    t.string   "label"
-    t.string   "pid"
+    t.string   "identifier", limit: 255
+    t.string   "model",      limit: 255
+    t.string   "label",      limit: 255
+    t.string   "pid",        limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "type"
-    t.boolean  "verified",   default: false
-    t.boolean  "handled",    default: false
-    t.boolean  "processed",  default: false
-    t.boolean  "validated",  default: false
+    t.string   "type",       limit: 255
+    t.boolean  "verified",               default: false
+    t.boolean  "handled",                default: false
+    t.boolean  "processed",              default: false
+    t.boolean  "validated",              default: false
   end
 
   add_index "batch_objects", ["verified"], name: "index_batch_objects_on_verified"
 
   create_table "batches", force: :cascade do |t|
-    t.string   "name"
-    t.string   "description"
+    t.string   "name",                  limit: 255
+    t.string   "description",           limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "user_id"
-    t.string   "status"
+    t.string   "status",                limit: 255
     t.datetime "start"
     t.datetime "stop"
-    t.string   "outcome"
-    t.string   "version"
-    t.string   "logfile_file_name"
-    t.string   "logfile_content_type"
+    t.string   "outcome",               limit: 255
+    t.string   "version",               limit: 255
+    t.string   "logfile_file_name",     limit: 255
+    t.string   "logfile_content_type",  limit: 255
     t.integer  "logfile_file_size"
     t.datetime "logfile_updated_at"
     t.datetime "processing_step_start"
@@ -102,13 +102,13 @@ ActiveRecord::Schema.define(version: 20171116183514) do
   end
 
   create_table "bookmarks", force: :cascade do |t|
-    t.integer  "user_id",       null: false
-    t.string   "user_type"
-    t.string   "document_id"
-    t.string   "title"
+    t.integer  "user_id",                   null: false
+    t.string   "user_type",     limit: 255
+    t.string   "document_id",   limit: 255
+    t.string   "title",         limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "document_type"
+    t.string   "document_type", limit: 255
   end
 
   add_index "bookmarks", ["user_id"], name: "index_bookmarks_on_user_id"
@@ -116,18 +116,18 @@ ActiveRecord::Schema.define(version: 20171116183514) do
   create_table "events", force: :cascade do |t|
     t.datetime "event_date_time"
     t.integer  "user_id"
-    t.string   "type"
-    t.string   "pid"
-    t.string   "software"
+    t.string   "type",            limit: 255
+    t.string   "pid",             limit: 255
+    t.string   "software",        limit: 255
     t.text     "comment"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "summary"
-    t.string   "outcome"
+    t.string   "summary",         limit: 255
+    t.string   "outcome",         limit: 255
     t.text     "detail"
     t.text     "exception",       limit: 65535
-    t.string   "user_key"
-    t.string   "permanent_id"
+    t.string   "user_key",        limit: 255
+    t.string   "permanent_id",    limit: 255
   end
 
   add_index "events", ["event_date_time"], name: "index_events_on_event_date_time"
@@ -139,7 +139,7 @@ ActiveRecord::Schema.define(version: 20171116183514) do
   create_table "searches", force: :cascade do |t|
     t.text     "query_params"
     t.integer  "user_id"
-    t.string   "user_type"
+    t.string   "user_type",    limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -147,24 +147,24 @@ ActiveRecord::Schema.define(version: 20171116183514) do
   add_index "searches", ["user_id"], name: "index_searches_on_user_id"
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
+    t.string   "email",                  limit: 255, default: "", null: false
+    t.string   "encrypted_password",     limit: 255, default: "", null: false
+    t.string   "reset_password_token",   limit: 255
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0
+    t.integer  "sign_in_count",                      default: 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
-    t.string   "username",               default: "", null: false
-    t.string   "first_name"
-    t.string   "middle_name"
-    t.string   "nickname"
-    t.string   "last_name"
-    t.string   "display_name"
+    t.string   "current_sign_in_ip",     limit: 255
+    t.string   "last_sign_in_ip",        limit: 255
+    t.datetime "created_at",                                      null: false
+    t.datetime "updated_at",                                      null: false
+    t.string   "username",               limit: 255, default: "", null: false
+    t.string   "first_name",             limit: 255
+    t.string   "middle_name",            limit: 255
+    t.string   "nickname",               limit: 255
+    t.string   "last_name",              limit: 255
+    t.string   "display_name",           limit: 255
   end
 
   add_index "users", ["email"], name: "index_users_on_email"
